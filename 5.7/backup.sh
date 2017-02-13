fc46076dd0773b6c2c71cdd0572dbfbc67d3f098
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2017-02-13 19:03:17 +1100

    use generic snapshots folder

--- 5.7/backup.sh
+++ 5.7/backup.sh
@@ -6,7 +6,7 @@
 set -e
 
 # Configuration
-BKUP_DIR=${BKUP_DIR:-"/var/lib/mysql/backups"}
+BKUP_DIR=${BKUP_DIR:-"/snapshots"}
 BKUP_RETENTION=${BKUP_RETENTION:-20}
 
 # Ensure backup directory exists
@@ -14,7 +14,8 @@ mkdir -p $BKUP_DIR
 
 # Perform backup
 ts=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
-mysqldump -u root -p$MYSQL_ROOT_PASSWORD --single-transaction --routines --triggers --all-databases > /dev/null 2>&1 | gzip > $BKUP_DIR/$ts.sql.gz
+mysqldump -u root -p$MYSQL_ROOT_PASSWORD --single-transaction --routines --triggers --all-databases > /dev/null 2>&1 | gzip > /tmp/$ts.sql.gz
+mv /tmp/$ts.sql.gz $BKUP_DIR/
 
 # Keep limited number of backups
 ls -1 $BKUP_DIR/*.sql.gz | head -n -${BKUP_RETENTION} | xargs -d '\n' rm -f --
